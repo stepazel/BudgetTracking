e678a43de28c08d5806c34b94adca41718d45ec6
stepazel <stepan.zelenka@seznam.cz>
2023-11-22 17:33:16 +0100

    added delete and total

--- BudgetTrackingApp/Controllers/HomeController.fs
+++ BudgetTrackingApp/Controllers/HomeController.fs
@@ -12,34 +12,43 @@ open BudgetTrackingApp.Models
 [<Authorize>]
 type HomeController(logger: ILogger<HomeController>) =
     inherit Controller()
-    member this.userId = this.HttpContext.User.Claims |> Seq.tryFind (fun claim -> claim.Type = "Id") |> Option.get |> fun claim -> claim.Value |> Convert.ToInt32
-    
+
+    member this.userId =
+        this.HttpContext.User.Claims
+        |> Seq.tryFind (fun claim -> claim.Type = "Id")
+        |> Option.get
+        |> fun claim -> claim.Value |> Convert.ToInt32
+
     member this.Index() =
         let connection = UserRepository.connection
         let command = connection.CreateCommand()
-        command.CommandText <- @"select description, amount, created, category from expenses where user_id = @user_id"
+        command.CommandText <- @"select description, amount, created, category, id from expenses where user_id = @user_id"
         command.Parameters.AddWithValue("user_id", this.userId) |> ignore
         use reader = command.ExecuteReader()
+
         let results: Expense seq =
             [ while reader.Read() do
                   yield
                       { Description = reader.GetString(0)
                         Amount = reader.GetInt32(1)
                         Created = reader.GetDateTime(2)
-                        Category = reader.GetString(3) } ]
+                        Category = reader.GetString(3)
+                        Id = reader.GetInt32(4) } ]
+
         reader.Close()
 
-        let (=>) x y = x,y
+        let (=>) x y = x, y
+
         this.View(
             { Expenses = results |> Seq.groupBy (fun expense -> expense.Category) |> Map.ofSeq
               CategoryNames =
                 Map
-                    [
-                      "groceries" => "Potraviny"
-                      "food"=> "Jídlo (restaurace)"
+                    [ "groceries" => "Potraviny"
+                      "food" => "Jídlo (restaurace)"
                       "housing" => "Bydlení"
                       "drinks" => "Pití (zbytné)"
-                      "transport" => "Doprava" ] }
+                      "transport" => "Doprava" ]
+              Total = results |> Seq.sumBy (fun expense -> expense.Amount)  }
         )
 
     member this.AddExpense(description: string, amount: int, category: string) =
@@ -60,6 +69,35 @@ type HomeController(logger: ILogger<HomeController>) =
         this.RedirectToAction("Index")
 
     member this.Privacy() = this.View()
+
+    [<HttpPost>]
+    member this.EditExpense(id: int, description: string, amount: int, category: string) =
+        let command = UserRepository.connection.CreateCommand()
+
+        command.CommandText <- "select user_id from expenses where id = @id"
+        command.Parameters.AddWithValue("@id", id) |> ignore
+
+        let userId = command.ExecuteScalar() |> Convert.ToInt32
+
+        if userId = this.userId then
+            command.CommandText <- "update expenses set (description, amount, category) = (@description, @amount, @category) where id = @expense_id"
+            command.Parameters.AddWithValue("@expense_id", id) |> ignore
+            command.Parameters.AddWithValue("@description", description) |> ignore
+            command.Parameters.AddWithValue("@amount", amount) |> ignore
+            command.Parameters.AddWithValue("@category", category) |> ignore
+            command.ExecuteNonQuery() |> ignore
+            this.RedirectToAction("index")
+        else
+            this.RedirectToAction("index")
+            
+    [<HttpPost>]
+    member this.DeleteExpense(id: int) =
+        let command = UserRepository.connection.CreateCommand()
+        command.CommandText <- "delete from expenses where id = @id and user_id = @user_id"
+        command.Parameters.AddWithValue("@id", id) |> ignore
+        command.Parameters.AddWithValue("@user_id", this.userId) |> ignore
+        command.ExecuteNonQuery() |> ignore
+        this.RedirectToAction("index")
 
     [<ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)>]
     member this.Error() =
--- BudgetTrackingApp/Models/HomepageModel.fs
+++ BudgetTrackingApp/Models/HomepageModel.fs
@@ -2,10 +2,10 @@
 
 open System
 
-type Expense = { Description: string; Amount: int; Created: DateTime; Category: string}
+type Expense = { Id: int; Description: string; Amount: int; Created: DateTime; Category: string}
 
 type KnownExpense = { Description: string }
 
 type CategoryNames = Map<string, string>
 
-type HomepageModel = { Expenses: Map<string, Expense seq>; CategoryNames: CategoryNames }
+type HomepageModel = { Expenses: Map<string, Expense seq>; CategoryNames: CategoryNames; Total: int }
