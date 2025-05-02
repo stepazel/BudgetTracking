cc8c4bb616cdd459e63942b0cf40d2ce1a15e3f1
stepazel <stepan.zelenka@seznam.cz>
2025-05-02 15:07:05 +0200

    added "inputted" info to expenses and displaying the last input date on homepage added Expenses page with all expenses changed datetime format to czech localization

--- BudgetTrackingApp/Models/HomepageModel.fs
+++ BudgetTrackingApp/Models/HomepageModel.fs
@@ -7,9 +7,24 @@ type KnownExpense = { Description: string }
 
 type Categories = Map<int, string>
 
-type Expense = {Id: int; Description: string; Amount: float; Created: DateTime; CategoryName: string}
+type Expense =
+    { Id: int
+      Description: string
+      Amount: float
+      Created: DateTime
+      CategoryName: string }
 
-type HomepageModel = { Expenses: IEnumerable<Expense>;Categories: Categories; Total: float; YearlyTotal: float; MonthlyTotal: float; WeeklyTotal: float }
+type HomepageModel =
+    { Expenses: IEnumerable<Expense>
+      Categories: Categories
+      Total: float
+      YearlyTotal: float
+      MonthlyTotal: float
+      WeeklyTotal: float
+      LastEntry: DateTime }
 
-type TotalsResult = { Total: float; YearlyTotal: float; MonthlyTotal: float; WeeklyTotal: float }
-
+type TotalsResult =
+    { Total: float
+      YearlyTotal: float
+      MonthlyTotal: float
+      WeeklyTotal: float }
